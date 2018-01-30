27a99217da3a5426e176c9f46271a3613ee6d4e4
crewsz96 <crewsz96@vt.edu>
2018-01-30 18:34:37 -0500

    Initial commit

---
+++ Assets/_Scripts/GameControllerScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControllerScript : MonoBehaviour {
+
+    private GameObject player;
+    private int currLevel = 1;
+    private int lastLevel = 1;
+    private GameObject[] cyanPlatforms;
+    private GameObject[] magentaPlatforms;
+    private GameObject[] yellowPlatforms;
+
+    private void Start() {
+
+        cyanPlatforms = GameObject.FindGameObjectsWithTag("Cyan Platform");
+        magentaPlatforms = GameObject.FindGameObjectsWithTag("Magenta Platform");
+        yellowPlatforms= GameObject.FindGameObjectsWithTag("Yellow Platform");
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        ChangeColor();
+
+        if (player == null)
+            LoadLevel();
+    }
+
+    private void ChangeColor()
+    {
+   
+
+
+
+    }
+
+    public void ColorCyan()
+    {
+        for (int i = 0; i < cyanPlatforms.Length; i++)
+        {
+            // Set Player to Cyan Sprite
+            // Set Set Cyan Platforms to Cyan
+            // Set all other Platforms to transparent?
+        }
+    }
+
+    public void M()
+    {
+
+    }
+
+    public void Y()
+    {
+
+    }
+
+    public void K()
+    {
+
+    }
+
+    public void LoadLevel()
+    {
+       if (currLevel <= lastLevel)
+            SceneManager.LoadScene("Level" + currLevel);
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed; // How fast Horizontal Movement is
+    public float jumpSpeed; // How fast the Short Jump is
+
+    enum jumpState {Grounded, hasJumped, hasDoubled};
+
+    private jumpState state;
+    private Rigidbody2D rb; // The player's rigidbody
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        state = jumpState.Grounded;
+    }
+
+    private void Update() {
+
+        move(); 
+        jump();
+    }
+
+    private void FixedUpdate() {
+
+       
+        
+    }
+
+    private void move() {
+
+        float move = Input.GetAxis("Horizontal");
+
+        Vector2 velocity = rb.velocity;
+        velocity.x = move * moveSpeed;
+        rb.velocity = velocity;
+
+    }
+
+    private void jump() {
+
+        if (Input.GetKeyDown("up"))
+        {
+            print(state);
+            if (state == jumpState.Grounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+                state = jumpState.hasJumped;
+
+                
+            }
+            else if (state == jumpState.hasJumped)
+            {
+                print("works");
+                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+                state = jumpState.hasDoubled;
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        state = jumpState.Grounded;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
